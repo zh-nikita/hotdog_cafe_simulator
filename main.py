52cf88d243b76fc9d81a851279ff9589470656aa
zh-nikita <nikita.zhovnir@icloud.com>
2025-10-28 00:01:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-from classes import Order, Items, Strategy, Strategy1, Strategy2, Strategy3, Director, NoSale, WithSale
-
-
-def main():
-    order = Order()
-    items = Items()
-    strategy = Strategy(order, items, Strategy1(order, items), Strategy2(order, items), Strategy3(order, items))
-    director = Director(order, items, strategy, NoSale(), WithSale())
-
-    while True:
-        option = input("First - Bavarian | Second - Regular | Custom - Custom Hotdog | Finish - Exit\nEnter: ").strip()
-
-        if option == "Custom":
-            additions = []
-            bread = input("Bread: ")
-            sausage = input("Sausage: ")
-            souse = input("Souse: ")
-            n = int(input("Number of additions: "))
-            for _ in range(n):
-                additions.append(input("Addition: "))
-            director.action(option, bread, sausage, souse, additions)
-
-        elif option == "Finish":
-            director.action(option, None, None, None, None)
-            break
-
-        else:
-            director.action(option, None, None, None, None)
-
+from hotdog_classes import Director
 
 if __name__ == "__main__":
-    main()
+    director = Director()
+    director.main()
